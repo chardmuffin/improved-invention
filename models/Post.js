8f5f53a346fa455867432c3e1a13bd74af118756
Richard Huffman <richardhuffman96@gmail.com>
2022-05-23 20:54:07 -0400

    fix various reference errors

--- models/Post.js
+++ models/Post.js
@@ -1,38 +1,38 @@
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('./config/config');
+const sequelize = require('../config/config');
 
 class Post extends Model {
-    // static async createPost(postData) {
-    //     return await this.create(postData);
-    // }
+    static async createPost(postData) {
+        return await this.create(postData);
+    }
 
-    // static async getPostById(postId) {
-    //     return await this.findOne({
-    //         where: {
-    //             id: postId,
-    //         },
-    //     });
-    // }
+    static async getPostById(postId) {
+        return await this.findOne({
+            where: {
+                id: postId,
+            },
+        });
+    }
 
-    // static async getPosts() {
-    //     return await this.findAll();
-    // }
+    static async getPosts() {
+        return await this.findAll();
+    }
 
-    // static async updatePost(postId, postData) {
-    //     return await this.update(postData, {
-    //         where: {
-    //             id: postId,
-    //         },
-    //     });
-    // }
+    static async updatePost(postId, postData) {
+        return await this.update(postData, {
+            where: {
+                id: postId,
+            },
+        });
+    }
 
-    // static async deletePost(postId) {
-    //     return await this.destroy({
-    //         where: {
-    //             id: postId,
-    //         },
-    //     });
-    // }
+    static async deletePost(postId) {
+        return await this.destroy({
+            where: {
+                id: postId,
+            },
+        });
+    }
 }
 
 Post.init(
